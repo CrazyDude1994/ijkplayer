9b05fb35d5fe3363e1ac26a1995b34f9fa49d94f
Kartavtsev Sergey <crazyserega1994@mail.ru>
2016-03-17 20:39:43 +0200

    test

--- ijkmedia/ijkplayer/ff_ffplay.c
+++ ijkmedia/ijkplayer/ff_ffplay.c
@@ -2769,7 +2769,7 @@ static int read_thread(void *arg)
                     if (!is->abort_request)
                         continue;
                 } else {
-                    ffp->auto_resume = 0;
+                    ffp->auto_resume = 1;
 
                     // TODO: 0 it's a bit early to notify complete here
                     ffp_toggle_buffering(ffp, 0);
